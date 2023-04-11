7b2bbd871668a2da2d3ec6b9cd3d622a700c69ce
cban1980 <cban@gmx.com>
2023-04-11 18:21:40 +0200

    make the script figure out all the users.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import paramiko
 from gsettings import Settings
 import dbus
+import subprocess
 
 # Load settings and nodes from a JSON file
 with open('settings.json') as f:
@@ -12,22 +13,30 @@ nodes = data['nodes']
 username = data['username']
 password = data['password']
 
-# Iterate over each node and schema and set the specified settings
+# Get a list of all user accounts on the system
+users = subprocess.check_output(["cut", "-d", ":", "-f", "1", "/etc/passwd"]).decode("utf-8").split("\n")
+
+# Iterate over each node, user, and schema and set the specified settings
 for node in nodes:
     # Connect to the node via SSH
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(node, username=username, password=password)
 
-    # Apply the specified settings for each schema
+    # Apply the specified settings for each schema and user
     bus = dbus.SystemBus()
     proxy = bus.get_object('org.gnome.SettingsDaemon', '/org/gnome/SettingsDaemon')
     for schema in schemas:
-        settings = Settings(schema=schema, bus_proxy=proxy)
-        for key, value in schemas[schema].items():
-            settings.set(key, value)
+        for user in users:
+            # Skip any user accounts that are empty or start with a system prefix
+            if user == "" or user.startswith("_") or user.startswith("systemd"):
+                continue
 
-        print(f"Settings applied for node {node} and schema {schema}")
+            settings = Settings(schema=schema, bus_proxy=proxy, path=f"/org/gnome/settings-daemon/plugins/{schema}/{user}")
+            for key, value in schemas[schema].items():
+                settings.set(key, value)
+
+            print(f"Settings applied for node {node}, user {user}, and schema {schema}")
 
     ssh.close()
 
